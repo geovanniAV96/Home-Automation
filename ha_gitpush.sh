95672426cf715c529b27d142b44d05812c78f281
root <root@core-ssh.local.hass.io>
2022-05-06 19:07:57 -0500

    first commit

---
+++ ha_gitpush.sh
@@ -0,0 +1,8 @@
+# Go to /config folder 
+cd /config
+# Add all files to the repository with respect to .gitignore rules
+git add .
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+# Push changes towards GitHub
+git push -u origin master
